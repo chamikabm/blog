2a0649ad03c052e3e4ea45476c071be78bb3e7aa
chamikabm <chamika@bitsmedia.com>
2019-12-02 17:02:36 +0800

    Refactored code to use the useState hook with blog post related states.

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const BlogContext = React.createContext();
 
 export const BlogProvider = ({ children }) => {
+  // const blogPosts = [ {title: 'Blog Post #1'}, {title: 'Blog Post #2'}];
+  // Without using the hardcoded state, we can make use of the react hook `useState` as follows.
+  const [blogPosts, setBlogPosts] = useState([]);
+
+  const addBlogPost = (blogPost) => {
+    setBlogPosts([...blogPosts, { title: `Blog Post ${blogPosts.length + 1}`}]);
+  };
+
 
   return (
-      <BlogContext.Provider value={15}>
+      <BlogContext.Provider
+          value={{
+            data: blogPosts,
+            addBlogPost,
+          }}
+      >
         {children}
       </BlogContext.Provider>
   );
