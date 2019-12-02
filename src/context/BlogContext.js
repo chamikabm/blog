1e7b56e4659106847b2fc8929276a7ca83e68a42
chamikabm <chamika@bitsmedia.com>
2019-12-02 17:29:36 +0800

    Refactored code to use the  hook.

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,22 +1,31 @@
-import React, { useState } from 'react';
+import React, { useReducer } from 'react';
 
 const BlogContext = React.createContext();
 
+const blogReducer = (state, action) => {
+
+  switch (action.type) {
+    case 'add_blogpost':
+      return [...state, { title: `Blog Post ${state.length + 1}`}];
+
+    default:
+      return state;
+
+  }
+};
+
 export const BlogProvider = ({ children }) => {
-  // const blogPosts = [ {title: 'Blog Post #1'}, {title: 'Blog Post #2'}];
-  // Without using the hardcoded state, we can make use of the react hook `useState` as follows.
-  const [blogPosts, setBlogPosts] = useState([]);
+  const [blogPosts, dispatch] = useReducer(blogReducer, []);
 
-  const addBlogPost = (blogPost) => {
-    setBlogPosts([...blogPosts, { title: `Blog Post ${blogPosts.length + 1}`}]);
+  const addBlogPost = () => {
+    dispatch({ type: 'add_blogpost' });
   };
-
 
   return (
       <BlogContext.Provider
           value={{
             data: blogPosts,
-            addBlogPost,
+            addBlogPost
           }}
       >
         {children}
