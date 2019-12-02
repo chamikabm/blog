c7c8660f53d2bf60ccd45e70e8bd86f71fc2cb55
chamikabm <chamika@bitsmedia.com>
2019-12-02 16:32:41 +0800

    Added context and provider to pass data to child props.

---
+++ src/context/BlogContext.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const BlogContext = React.createContext();
+
+export const BlogProvider = ({ children }) => {
+
+  return (
+      <BlogContext.Provider value={15}>
+        {children}
+      </BlogContext.Provider>
+  );
+};
+
+export default BlogContext;
