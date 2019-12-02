b732327d188c65f8bf472e34827fcaad7afa1250
chamikabm <chamika@bitsmedia.com>
2019-12-02 18:33:08 +0800

    Blog post show screen added.

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -5,7 +5,7 @@ import {
 import { Context } from '../context/BlogContext';
 import { Feather } from '@expo/vector-icons';
 
-const IndexScreen = () => {
+const IndexScreen = ({ navigation }) => {
   const { state, addBlogPost, deleteBlogPost} = useContext(Context);
 
   return (
@@ -20,6 +20,11 @@ const IndexScreen = () => {
             keyExtractor={(blogPost) => blogPost.title}
             renderItem={({ item }) => {
               return (
+                <TouchableOpacity
+                    onPress={
+                      () => navigation.navigate('Show', { id: item.id })
+                    }
+                >
                   <View style={styles.row}>
                     <Text style={styles.title}>{item.title} - {item.id}</Text>
                     <TouchableOpacity
@@ -28,6 +33,7 @@ const IndexScreen = () => {
                       <Feather style={styles.icon} name={'trash'}/>
                     </TouchableOpacity>
                   </View>
+                </TouchableOpacity>
               );
             }}
         />
